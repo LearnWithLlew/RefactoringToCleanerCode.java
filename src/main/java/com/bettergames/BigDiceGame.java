ccb99c13d6c4a1d146fe6fd547d1ec4841680354
Administrator <me@gail.com>
2021-07-28 12:44:49 +0000

    r - extract method doSomeNextThing5

--- src/main/java/com/bettergames/BigDiceGame.java
+++ src/main/java/com/bettergames/BigDiceGame.java
@@ -19,23 +19,25 @@ public class BigDiceGame
                 return doSomethingOnCodingFestival(die1, die2, die3, die4, die5);
 
             case FiveOfAKind:
+                return doSomeNextThing5(die1, die2, die3, die4, die5);
+        }
+
+        return 0;
+    }
+
+    private static int doSomeNextThing5(int die1, int die2, int die3, int die4, int die5) {
+        int[] dice1 = {die1, die2, die3, die4, die5};
+        var counts2 = new int[6];
+        for (var die : dice1)
+        {
+            counts2[die - 1]++;
+        }
+
+        for (var i1 = 0; i1 != 6; i1++)
+        {
+            if (counts2[i1] == 5)
             {
-                int[] dice1 = {die1, die2, die3, die4, die5};
-                var counts2 = new int[6];
-                for (var die : dice1)
-                {
-                    counts2[die - 1]++;
-                }
-
-                for (var i1 = 0; i1 != 6; i1++)
-                {
-                    if (counts2[i1] == 5)
-                    {
-                        return BIG_SCORE;
-                    }
-                }
-
-                return 0;
+                return BIG_SCORE;
             }
         }
 
