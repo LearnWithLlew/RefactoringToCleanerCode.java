c46f5c8d85897cdd033a275f604fc2190e61f4ac
Administrator <me@gail.com>
2021-07-28 13:00:21 +0000

    r - extract method to FiveOfAKindCalculator

---
+++ src/main/java/com/bettergames/FiveOfAKindCalculator.java
@@ -0,0 +1,22 @@
+package com.bettergames;
+
+public class FiveOfAKindCalculator {
+    public static int doSomeNextThing5(int die1, int die2, int die3, int die4, int die5) {
+        int[] dice1 = {die1, die2, die3, die4, die5};
+        var counts2 = new int[6];
+        for (var die : dice1)
+        {
+            counts2[die - 1]++;
+        }
+
+        for (var i1 = 0; i1 != 6; i1++)
+        {
+            if (counts2[i1] == 5)
+            {
+                return BigDiceGame.BIG_SCORE;
+            }
+        }
+
+        return 0;
+    }
+}
