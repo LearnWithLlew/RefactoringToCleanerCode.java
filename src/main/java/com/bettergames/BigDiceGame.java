a0e47076f70c0bcbf556d24794b8fba3f72e3321
Administrator <me@gail.com>
2021-07-28 13:21:43 +0000

    r - make non-static

--- src/main/java/com/bettergames/BigDiceGame.java
+++ src/main/java/com/bettergames/BigDiceGame.java
@@ -14,7 +14,7 @@ public class BigDiceGame
         }
         {
             if (new TwoPairCalculator().isScorable(scoringType)) {
-                return TwoPairCalculator.calculateScore(die1, die2, die3, die4, die5);
+                return new TwoPairCalculator().calculateScore(die1, die2, die3, die4, die5);
             }
         }
         {
