4a29f72a053c4dd8215665a84bdfe4a4842970b0
Administrator <me@gail.com>
2021-07-28 13:18:50 +0000

    R!! - sound with {}

--- src/main/java/com/bettergames/BigDiceGame.java
+++ src/main/java/com/bettergames/BigDiceGame.java
@@ -7,17 +7,25 @@ public class BigDiceGame
 
     public static int calculateScore(ScoringType scoringType, int die1, int die2, int die3, int die4, int die5)
     {
-        if (PairCalculator.isScorable(scoringType)) {
-            return PairCalculator.calculateScore(die1, die2, die3, die4, die5);
+        {
+            if (PairCalculator.isScorable(scoringType)) {
+                return PairCalculator.calculateScore(die1, die2, die3, die4, die5);
+            }
         }
-        if (TwoPairCalculator.isScorable(scoringType)) {
-            return TwoPairCalculator.calculateScore(die1, die2, die3, die4, die5);
+        {
+            if (TwoPairCalculator.isScorable(scoringType)) {
+                return TwoPairCalculator.calculateScore(die1, die2, die3, die4, die5);
+            }
         }
-        if (FullHouseCalculator.isScorable(scoringType)) {
-            return FullHouseCalculator.calculateScore(die1, die2, die3, die4, die5);
+        {
+            if (FullHouseCalculator.isScorable(scoringType)) {
+                return FullHouseCalculator.calculateScore(die1, die2, die3, die4, die5);
+            }
         }
-        if (FiveOfAKindCalculator.isScorable(scoringType)) {
-            return FiveOfAKindCalculator.calculateScore(die1, die2, die3, die4, die5);
+        {
+            if (FiveOfAKindCalculator.isScorable(scoringType)) {
+                return FiveOfAKindCalculator.calculateScore(die1, die2, die3, die4, die5);
+            }
         }
 
         return 0;
