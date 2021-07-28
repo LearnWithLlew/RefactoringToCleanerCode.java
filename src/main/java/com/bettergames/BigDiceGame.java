f3a2ed8e2e4e7b1632061354fbf76e3f7f200658
Administrator <me@gail.com>
2021-07-28 12:50:12 +0000

    r - extract method to PairCalculator

--- src/main/java/com/bettergames/BigDiceGame.java
+++ src/main/java/com/bettergames/BigDiceGame.java
@@ -10,7 +10,7 @@ public class BigDiceGame
         switch (scoringType)
         {
             case Pair:
-                return doSomething(die1, die2, die3, die4, die5);
+                return PairCalculator.doSomething(die1, die2, die3, die4, die5);
 
             case TwoPair:
                 return doAnotherThing(die1, die2, die3, die4, die5);
@@ -111,23 +111,4 @@ public class BigDiceGame
         return 0;
     }
 
-    private static int doSomething(int die1, int die2, int die3, int die4, int die5) {
-        final int value = 2;
-        int[] tallies1;
-        tallies1 = new int[6];
-        tallies1[die1 - 1]++;
-        tallies1[die2 - 1]++;
-        tallies1[die3 - 1]++;
-        tallies1[die4 - 1]++;
-        tallies1[die5 - 1]++;
-        for (var i = 6 - 1; i >= 0; i--)
-        {
-            if (tallies1[i] >= value)
-            {
-                return (i + 1) * value;
-            }
-        }
-
-        return 0;
-    }
 }
