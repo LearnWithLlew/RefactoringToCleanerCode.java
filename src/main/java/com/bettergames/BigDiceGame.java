8c6e197402d38c8d04e332d48f075fb6bb80cbab
Administrator <me@gail.com>
2021-07-28 13:30:35 +0000

    R!! - Many of Three

--- src/main/java/com/bettergames/BigDiceGame.java
+++ src/main/java/com/bettergames/BigDiceGame.java
@@ -10,18 +10,13 @@ public class BigDiceGame
     public static int calculateScore(ScoringType scoringType, int die1, int die2, int die3, int die4, int die5)
     {
         {
-            for(ScoreCalculator calculator : List.of(new PairCalculator(), new TwoPairCalculator())){
+            for(ScoreCalculator calculator : List.of(new PairCalculator(), new TwoPairCalculator(), new FullHouseCalculator())){
             if (calculator.isScorable(scoringType)) {
                 return calculator.calculateScore(die1, die2, die3, die4, die5);
             }}
         }
 
-        {
-            ScoreCalculator calculator = new FullHouseCalculator();
-            if (calculator.isScorable(scoringType)) {
-                return calculator.calculateScore(die1, die2, die3, die4, die5);
-            }
-        }
+
         {
             ScoreCalculator calculator = new FiveOfAKindCalculator();
             if (calculator.isScorable(scoringType)) {
