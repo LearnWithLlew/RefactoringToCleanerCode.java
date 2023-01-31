af29bc43a03dfffd85ede266ab0562a71cabb8ae
Llewellyn Falco <llewellyn.falco@gmail.com>
2023-01-31 09:28:59 -0500

    r extract strawberryJam

--- src/main/java/com/bettergames/BigDiceGame.java
+++ src/main/java/com/bettergames/BigDiceGame.java
@@ -14,29 +14,7 @@ public class BigDiceGame
 
             case TwoPair:
             {
-                var counts1 = new int[6];
-                counts1[die1 - 1]++;
-                counts1[die2 - 1]++;
-                counts1[die3 - 1]++;
-                counts1[die4 - 1]++;
-                counts1[die5 - 1]++;
-                var n = 0;
-                var score = 0;
-                for (var i = 0; i < 6; i += 1)
-                {
-                    if (counts1[6 - i - 1] >= 2)
-                    {
-                        n++;
-                        score += 6 - i;
-                    }
-                }
-
-                if (n == 2)
-                {
-                    return score * 2;
-                }
-
-                return 0;
+                return strawberryJam(die1, die2, die3, die4, die5);
             }
 
             case FullHouse:
@@ -100,6 +78,32 @@ public class BigDiceGame
 
                 return 0;
             }
+        }
+
+        return 0;
+    }
+
+    private static int strawberryJam(int die1, int die2, int die3, int die4, int die5) {
+        var counts1 = new int[6];
+        counts1[die1 - 1]++;
+        counts1[die2 - 1]++;
+        counts1[die3 - 1]++;
+        counts1[die4 - 1]++;
+        counts1[die5 - 1]++;
+        var n = 0;
+        var score = 0;
+        for (var i = 0; i < 6; i += 1)
+        {
+            if (counts1[6 - i - 1] >= 2)
+            {
+                n++;
+                score += 6 - i;
+            }
+        }
+
+        if (n == 2)
+        {
+            return score * 2;
         }
 
         return 0;
