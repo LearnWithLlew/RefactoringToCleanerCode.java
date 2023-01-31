cef2cbb4ca1549853b95e234c39ca256d9a6af39
Llewellyn Falco <llewellyn.falco@gmail.com>
2023-01-31 09:33:51 -0500

    r undid function extraction

--- src/main/java/com/bettergames/BigDiceGame.java
+++ src/main/java/com/bettergames/BigDiceGame.java
@@ -24,26 +24,22 @@ public class BigDiceGame
 
             case FiveOfAKind:
             {
-                return blackCurrantJam(die1, die2, die3, die4, die5, bigScore);
-            }
-        }
+                int[] dice1 = {die1, die2, die3, die4, die5};
+                var counts2 = new int[6];
+                for (var die : dice1)
+                {
+                    counts2[die - 1]++;
+                }
 
-        return 0;
-    }
+                for (var i1 = 0; i1 != 6; i1++)
+                {
+                    if (counts2[i1] == 5)
+                    {
+                        return bigScore;
+                    }
+                }
 
-    private static int blackCurrantJam(int die1, int die2, int die3, int die4, int die5, int bigScore) {
-        int[] dice1 = {die1, die2, die3, die4, die5};
-        var counts2 = new int[6];
-        for (var die : dice1)
-        {
-            counts2[die - 1]++;
-        }
-
-        for (var i1 = 0; i1 != 6; i1++)
-        {
-            if (counts2[i1] == 5)
-            {
-                return bigScore;
+                return 0;
             }
         }
 
