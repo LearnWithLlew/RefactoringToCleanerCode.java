87c39fbde84e0af00ac29c7d01d0f675df63dd56
Llewellyn Falco <llewellyn.falco@gmail.com>
2023-01-31 10:31:11 -0500

    r repalced switch with if

--- src/main/java/com/bettergames/BigDiceGame.java
+++ src/main/java/com/bettergames/BigDiceGame.java
@@ -4,27 +4,14 @@ public class BigDiceGame
 {
     public static int calculateScore(ScoringType st, int die1, int die2, int die3, int die4, int die5)
     {
-        switch (st)
-        {
-            case Pair:
-            {
-                return new Pair().score(die1, die2, die3, die4, die5);
-            }
-
-            case TwoPair:
-            {
-                return new TwoPair().score(die1, die2, die3, die4, die5);
-            }
-
-            case FullHouse:
-            {
-                return new FullHouse().score(die1, die2, die3, die4, die5);
-            }
-
-            case FiveOfAKind:
-            {
-                return new FiveOfAKind().score(die1, die2, die3, die4, die5);
-            }
+        if (st == ScoringType.Pair) {
+            return new Pair().score(die1, die2, die3, die4, die5);
+        } else if (st == ScoringType.TwoPair) {
+            return new TwoPair().score(die1, die2, die3, die4, die5);
+        } else if (st == ScoringType.FullHouse) {
+            return new FullHouse().score(die1, die2, die3, die4, die5);
+        } else if (st == ScoringType.FiveOfAKind) {
+            return new FiveOfAKind().score(die1, die2, die3, die4, die5);
         }
 
         return 0;
