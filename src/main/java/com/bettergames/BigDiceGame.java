5f576b495664d2e8dec783353880f745a8bff63c
Llewellyn Falco <llewellyn.falco@gmail.com>
2023-01-31 11:31:36 -0500

    R!! removed dead code

--- src/main/java/com/bettergames/BigDiceGame.java
+++ src/main/java/com/bettergames/BigDiceGame.java
@@ -3,7 +3,6 @@ package com.bettergames;
 public class BigDiceGame {
     public static int calculateScore(ScoringType st, int die1, int die2, int die3, int die4, int die5) {
         Scorer pair[] = {new Pair(), new TwoPair()};
-        TwoPair twoPair = new TwoPair();
         FullHouse fullHouse = new FullHouse();
         FiveOfAKind fiveOfAKind = new FiveOfAKind();
         for (Scorer pair1 : pair) {
@@ -11,9 +10,7 @@ public class BigDiceGame {
                 return pair1.score(die1, die2, die3, die4, die5);
             }
         }
-        if (twoPair.isScorable(st)) {
-            return twoPair.score(die1, die2, die3, die4, die5);
-        } else {
+        {
             if (fullHouse.isScorable(st)) {
                 return fullHouse.score(die1, die2, die3, die4, die5);
             } else {
