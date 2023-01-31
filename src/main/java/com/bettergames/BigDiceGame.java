4aff29149f5df2679cd5e4ab19fe806be9c5d12d
Llewellyn Falco <llewellyn.falco@gmail.com>
2023-01-31 11:01:13 -0500

    r extract variable pair

--- src/main/java/com/bettergames/BigDiceGame.java
+++ src/main/java/com/bettergames/BigDiceGame.java
@@ -4,8 +4,9 @@ public class BigDiceGame
 {
     public static int calculateScore(ScoringType st, int die1, int die2, int die3, int die4, int die5)
     {
-        if (new Pair().isScorable(st)) {
-            return new Pair().score(die1, die2, die3, die4, die5);
+        Pair pair = new Pair();
+        if (pair.isScorable(st)) {
+            return pair.score(die1, die2, die3, die4, die5);
         } else if (new TwoPair().isScorable(st)) {
             return new TwoPair().score(die1, die2, die3, die4, die5);
         } else if (new FullHouse().isScorable(st)) {
