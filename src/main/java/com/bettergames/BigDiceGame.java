10c97145b35b0dd62cf69701cf82e401c2f080ed
Llewellyn Falco <llewellyn.falco@gmail.com>
2023-01-31 11:23:10 -0500

    R!! many of 1

--- src/main/java/com/bettergames/BigDiceGame.java
+++ src/main/java/com/bettergames/BigDiceGame.java
@@ -2,12 +2,12 @@ package com.bettergames;
 
 public class BigDiceGame {
     public static int calculateScore(ScoringType st, int die1, int die2, int die3, int die4, int die5) {
-        Pair pair = new Pair();
+        Pair pair[] = {new Pair()};
         TwoPair twoPair = new TwoPair();
         FullHouse fullHouse = new FullHouse();
         FiveOfAKind fiveOfAKind = new FiveOfAKind();
-        if (pair.isScorable(st)) {
-            return pair.score(die1, die2, die3, die4, die5);
+        if (pair[0].isScorable(st)) {
+            return pair[0].score(die1, die2, die3, die4, die5);
         }
         if (twoPair.isScorable(st)) {
             return twoPair.score(die1, die2, die3, die4, die5);
