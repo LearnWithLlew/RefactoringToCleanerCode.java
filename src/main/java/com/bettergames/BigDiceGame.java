1259fc49c7a04d5dc9e5e5a3ec7c435890f5ef29
Llewellyn Falco <llewellyn.falco@gmail.com>
2023-01-31 10:53:45 -0500

    r unstatic isScorable

--- src/main/java/com/bettergames/BigDiceGame.java
+++ src/main/java/com/bettergames/BigDiceGame.java
@@ -4,7 +4,7 @@ public class BigDiceGame
 {
     public static int calculateScore(ScoringType st, int die1, int die2, int die3, int die4, int die5)
     {
-        if (Pair.isScorable(st)) {
+        if (new Pair().isScorable(st)) {
             return new Pair().score(die1, die2, die3, die4, die5);
         } else if (TwoPair.isScorable(st)) {
             return new TwoPair().score(die1, die2, die3, die4, die5);
--- src/main/java/com/bettergames/Pair.java
+++ src/main/java/com/bettergames/Pair.java
@@ -1,7 +1,7 @@
 package com.bettergames;
 
 public class Pair implements Scorer {
-    public static boolean isScorable(ScoringType st) {
+    public boolean isScorable(ScoringType st) {
         return st == ScoringType.Pair;
     }
 
