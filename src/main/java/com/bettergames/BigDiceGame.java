c4e1754290c289048bb973d8bd7d8afd1a33bf50
Llewellyn Falco <llewellyn.falco@gmail.com>
2023-01-31 10:35:33 -0500

    r extracted Method

--- src/main/java/com/bettergames/BigDiceGame.java
+++ src/main/java/com/bettergames/BigDiceGame.java
@@ -4,7 +4,7 @@ public class BigDiceGame
 {
     public static int calculateScore(ScoringType st, int die1, int die2, int die3, int die4, int die5)
     {
-        if (st == ScoringType.Pair) {
+        if (isPair(st)) {
             return new Pair().score(die1, die2, die3, die4, die5);
         } else if (st == ScoringType.TwoPair) {
             return new TwoPair().score(die1, die2, die3, die4, die5);
@@ -15,6 +15,10 @@ public class BigDiceGame
         }
 
         return 0;
+    }
+
+    private static boolean isPair(ScoringType st) {
+        return st == ScoringType.Pair;
     }
 
 }
