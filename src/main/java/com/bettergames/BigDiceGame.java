af2f1518cad764e9ef111e161452a3260224dd30
Llewellyn Falco <llewellyn.falco@gmail.com>
2023-01-31 11:05:49 -0500

    r extract twoPair variable

--- src/main/java/com/bettergames/BigDiceGame.java
+++ src/main/java/com/bettergames/BigDiceGame.java
@@ -1,18 +1,19 @@
 package com.bettergames;
 
-public class BigDiceGame
-{
-    public static int calculateScore(ScoringType st, int die1, int die2, int die3, int die4, int die5)
-    {
+public class BigDiceGame {
+    public static int calculateScore(ScoringType st, int die1, int die2, int die3, int die4, int die5) {
         Pair pair = new Pair();
+        TwoPair twoPair = new TwoPair();
         if (pair.isScorable(st)) {
             return pair.score(die1, die2, die3, die4, die5);
-        } else if (new TwoPair().isScorable(st)) {
-            return new TwoPair().score(die1, die2, die3, die4, die5);
-        } else if (new FullHouse().isScorable(st)) {
-            return new FullHouse().score(die1, die2, die3, die4, die5);
-        } else if (new FiveOfAKind().isScorable(st)) {
-            return new FiveOfAKind().score(die1, die2, die3, die4, die5);
+        } else {
+            if (twoPair.isScorable(st)) {
+                return twoPair.score(die1, die2, die3, die4, die5);
+            } else if (new FullHouse().isScorable(st)) {
+                return new FullHouse().score(die1, die2, die3, die4, die5);
+            } else if (new FiveOfAKind().isScorable(st)) {
+                return new FiveOfAKind().score(die1, die2, die3, die4, die5);
+            }
         }
 
         return 0;
