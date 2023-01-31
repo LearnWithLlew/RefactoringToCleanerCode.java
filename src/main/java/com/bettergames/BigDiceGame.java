eda08ae5f2b27940241a9da9681c225b49600bd5
Llewellyn Falco <llewellyn.falco@gmail.com>
2023-01-31 09:30:15 -0500

    r - extracted function

--- src/main/java/com/bettergames/BigDiceGame.java
+++ src/main/java/com/bettergames/BigDiceGame.java
@@ -19,44 +19,7 @@ public class BigDiceGame
 
             case FullHouse:
             {
-                int[] tallies;
-                var _2 = false;
-                int i;
-                var _2_at = 0;
-                var _3 = false;
-                var _3_at = 0;
-
-                tallies = new int[6];
-                tallies[die1 - 1] += 1;
-                tallies[die2 - 1] += 1;
-                tallies[die3 - 1] += 1;
-                tallies[die4 - 1] += 1;
-                tallies[die5 - 1] += 1;
-
-                for (i = 0; i != 6; i += 1)
-                {
-                    if (tallies[i] == 2)
-                    {
-                        _2 = true;
-                        _2_at = i + 1;
-                    }
-                }
-
-                for (i = 0; i != 6; i += 1)
-                {
-                    if (tallies[i] == 3)
-                    {
-                        _3 = true;
-                        _3_at = i + 1;
-                    }
-                }
-
-                if (_2 && _3)
-                {
-                    return _2_at * 2 + _3_at * 3;
-                }
-
-                return 0;
+                return grapeJam(die1, die2, die3, die4, die5);
             }
 
             case FiveOfAKind:
@@ -78,6 +41,47 @@ public class BigDiceGame
 
                 return 0;
             }
+        }
+
+        return 0;
+    }
+
+    private static int grapeJam(int die1, int die2, int die3, int die4, int die5) {
+        int[] tallies;
+        var _2 = false;
+        int i;
+        var _2_at = 0;
+        var _3 = false;
+        var _3_at = 0;
+
+        tallies = new int[6];
+        tallies[die1 - 1] += 1;
+        tallies[die2 - 1] += 1;
+        tallies[die3 - 1] += 1;
+        tallies[die4 - 1] += 1;
+        tallies[die5 - 1] += 1;
+
+        for (i = 0; i != 6; i += 1)
+        {
+            if (tallies[i] == 2)
+            {
+                _2 = true;
+                _2_at = i + 1;
+            }
+        }
+
+        for (i = 0; i != 6; i += 1)
+        {
+            if (tallies[i] == 3)
+            {
+                _3 = true;
+                _3_at = i + 1;
+            }
+        }
+
+        if (_2 && _3)
+        {
+            return _2_at * 2 + _3_at * 3;
         }
 
         return 0;
