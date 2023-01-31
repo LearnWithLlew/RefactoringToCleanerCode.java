368df08272903ed9742900b07f7be9619248c8eb
Llewellyn Falco <llewellyn.falco@gmail.com>
2023-01-31 11:09:21 -0500

    r store fullHouse as variable

--- src/main/java/com/bettergames/BigDiceGame.java
+++ src/main/java/com/bettergames/BigDiceGame.java
@@ -4,15 +4,18 @@ public class BigDiceGame {
     public static int calculateScore(ScoringType st, int die1, int die2, int die3, int die4, int die5) {
         Pair pair = new Pair();
         TwoPair twoPair = new TwoPair();
+        FullHouse fullHouse = new FullHouse();
         if (pair.isScorable(st)) {
             return pair.score(die1, die2, die3, die4, die5);
         } else {
             if (twoPair.isScorable(st)) {
                 return twoPair.score(die1, die2, die3, die4, die5);
-            } else if (new FullHouse().isScorable(st)) {
-                return new FullHouse().score(die1, die2, die3, die4, die5);
-            } else if (new FiveOfAKind().isScorable(st)) {
-                return new FiveOfAKind().score(die1, die2, die3, die4, die5);
+            } else {
+                if (fullHouse.isScorable(st)) {
+                    return fullHouse.score(die1, die2, die3, die4, die5);
+                } else if (new FiveOfAKind().isScorable(st)) {
+                    return new FiveOfAKind().score(die1, die2, die3, die4, die5);
+                }
             }
         }
 
