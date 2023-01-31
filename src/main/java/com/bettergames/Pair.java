4b978a986b42e9819385a4017bf60bc80e9de7b9
Llewellyn Falco <llewellyn.falco@gmail.com>
2023-01-31 10:27:39 -0500

    r implement interface Scorer

--- src/main/java/com/bettergames/Pair.java
+++ src/main/java/com/bettergames/Pair.java
@@ -1,6 +1,6 @@
 package com.bettergames;
 
-public class Pair {
+public class Pair implements Scorer {
     public int score(int die1, int die2, int die3, int die4, int die5) {
         final int value = 2;
         int[] tallies1;
