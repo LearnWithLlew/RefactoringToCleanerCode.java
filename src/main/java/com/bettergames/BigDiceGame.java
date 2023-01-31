5d1092d932474db4aa85073c177c24d3c022b29f
Llewellyn Falco <llewellyn.falco@gmail.com>
2023-01-31 10:17:35 -0500

    r move fiveOfAKindScore into FiveOfAKind

--- src/main/java/com/bettergames/BigDiceGame.java
+++ src/main/java/com/bettergames/BigDiceGame.java
@@ -23,26 +23,7 @@ public class BigDiceGame
 
             case FiveOfAKind:
             {
-                return fiveOfAKindScore(die1, die2, die3, die4, die5);
-            }
-        }
-
-        return 0;
-    }
-
-    private static int fiveOfAKindScore(int die1, int die2, int die3, int die4, int die5) {
-        int[] dice1 = {die1, die2, die3, die4, die5};
-        var counts2 = new int[6];
-        for (var die : dice1)
-        {
-            counts2[die - 1]++;
-        }
-
-        for (var i1 = 0; i1 != 6; i1++)
-        {
-            if (counts2[i1] == 5)
-            {
-                return 50;
+                return FiveOfAKind.fiveOfAKindScore(die1, die2, die3, die4, die5);
             }
         }
 
