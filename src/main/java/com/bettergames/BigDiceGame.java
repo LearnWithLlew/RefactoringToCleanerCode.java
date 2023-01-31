7fa87d36df99a2633d5906ee7b0cbf73ccbb320f
Llewellyn Falco <llewellyn.falco@gmail.com>
2023-01-31 11:26:03 -0500

    R!! loops over pair

--- src/main/java/com/bettergames/BigDiceGame.java
+++ src/main/java/com/bettergames/BigDiceGame.java
@@ -6,8 +6,10 @@ public class BigDiceGame {
         TwoPair twoPair = new TwoPair();
         FullHouse fullHouse = new FullHouse();
         FiveOfAKind fiveOfAKind = new FiveOfAKind();
-        if (pair[0].isScorable(st)) {
-            return pair[0].score(die1, die2, die3, die4, die5);
+        for (Pair pair1 : pair) {
+            if (pair1.isScorable(st)) {
+                return pair1.score(die1, die2, die3, die4, die5);
+            }
         }
         if (twoPair.isScorable(st)) {
             return twoPair.score(die1, die2, die3, die4, die5);
