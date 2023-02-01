0bac7b7e41bab208af26988989526aacc4831d43
Llewellyn Falco <llewellyn.falco@gmail.com>
2023-02-01 08:43:10 -0500

    r to stream

--- src/main/java/com/bettergames/BigDiceGame.java
+++ src/main/java/com/bettergames/BigDiceGame.java
@@ -1,15 +1,17 @@
 package com.bettergames;
 
+import java.util.Arrays;
+
 public class BigDiceGame {
     public static int calculateScore(ScoringType st, int die1, int die2, int die3, int die4, int die5) {
         Scorer pair[] = {new Pair(), new TwoPair(), new FullHouse(), new FiveOfAKind()};
-        for (Scorer pair1 : pair) {
-            if (pair1.isScorable(st)) {
-                return pair1.score(die1, die2, die3, die4, die5);
-            }
-        }
+        return Arrays.
+                stream(pair)
+                .filter(pair1 -> pair1.isScorable(st))
+                .findFirst()
+                .map(pair1 -> pair1.score(die1, die2, die3, die4, die5))
+                .orElse(0);
 
-        return 0;
     }
 
 }
