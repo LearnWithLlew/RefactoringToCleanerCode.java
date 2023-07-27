99166fb6b0333ef178009b1e89b91aee6098a608
Llewellyn Falco <llewellyn.falco@gmail.com>
2023-07-27 16:30:19 -0400

    . r Extract Method

--- src/main/java/com/bettergames/BigDiceGame.java
+++ src/main/java/com/bettergames/BigDiceGame.java
@@ -5,7 +5,7 @@ public class BigDiceGame
     public static int calculateScore(ScoringType scoringType, int die1, int die2, int die3, int die4, int die5)
     {
         {
-            var scorer = new DiceScorer[]{new PairScore(), new TwoPairScore(), new FullHouseScore(), new FiveOfAKindScore()};
+            DiceScorer[] scorer = getScoringTypes();
             for (DiceScorer diceScorer : scorer) {
                 if (diceScorer.isScore(scoringType)) {
                     return diceScorer.calculateScore(die1, die2, die3, die4, die5);
@@ -15,6 +15,11 @@ public class BigDiceGame
         }
 
         return 0;
+    }
+
+    private static DiceScorer[] getScoringTypes() {
+        var scorer = new DiceScorer[]{new PairScore(), new TwoPairScore(), new FullHouseScore(), new FiveOfAKindScore()};
+        return scorer;
     }
 
 }
