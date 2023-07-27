6af3389b2b5127a6077d084fcb4ea9d4ae720915
Llewellyn Falco <llewellyn.falco@gmail.com>
2023-07-27 14:50:40 -0400

    . r create fullhouse class

--- src/main/java/com/bettergames/BigDiceGame.java
+++ src/main/java/com/bettergames/BigDiceGame.java
@@ -13,7 +13,7 @@ public class BigDiceGame
                 return TwoPairScore.applesauce2(die1, die2, die3, die4, die5);
 
             case FullHouse:
-                return applesauce3(die1, die2, die3, die4, die5);
+                return FullHouseScore.applesauce3(die1, die2, die3, die4, die5);
 
             case FiveOfAKind:
                 return applesauce4(die1, die2, die3, die4, die5);
@@ -38,47 +38,6 @@ public class BigDiceGame
                 int bigScore = 50;
                 return bigScore;
             }
-        }
-
-        return 0;
-    }
-
-    private static int applesauce3(int die1, int die2, int die3, int die4, int die5) {
-        int[] tallies;
-        var _2 = false;
-        int i;
-        var _2_at = 0;
-        var _3 = false;
-        var _3_at = 0;
-
-        tallies = new int[6];
-        tallies[die1 - 1] += 1;
-        tallies[die2 - 1] += 1;
-        tallies[die3 - 1] += 1;
-        tallies[die4 - 1] += 1;
-        tallies[die5 - 1] += 1;
-
-        for (i = 0; i != 6; i += 1)
-        {
-            if (tallies[i] == 2)
-            {
-                _2 = true;
-                _2_at = i + 1;
-            }
-        }
-
-        for (i = 0; i != 6; i += 1)
-        {
-            if (tallies[i] == 3)
-            {
-                _3 = true;
-                _3_at = i + 1;
-            }
-        }
-
-        if (_2 && _3)
-        {
-            return _2_at * 2 + _3_at * 3;
         }
 
         return 0;
