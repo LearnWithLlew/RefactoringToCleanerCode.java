231aaaa288996e31beb072bc28737bbe10336eeb
Llewellyn Falco <llewellyn.falco@gmail.com>
2023-07-27 14:41:53 -0400

    . r Rename

--- src/main/java/com/bettergames/BigDiceGame.java
+++ src/main/java/com/bettergames/BigDiceGame.java
@@ -2,9 +2,9 @@ package com.bettergames;
 
 public class BigDiceGame
 {
-    public static int calculateScore(ScoringType st, int die1, int die2, int die3, int die4, int die5)
+    public static int calculateScore(ScoringType scoringType, int die1, int die2, int die3, int die4, int die5)
     {
-        switch (st)
+        switch (scoringType)
         {
             case Pair:
                 return applesauce(die1, die2, die3, die4, die5);
