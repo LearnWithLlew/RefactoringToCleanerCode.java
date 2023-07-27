2f7f596bc74537b6bd241d9af2c78c4c1daaf41e
Llewellyn Falco <llewellyn.falco@gmail.com>
2023-07-27 14:36:21 -0400

    . r extracted applesauce4 logic

--- src/main/java/com/bettergames/BigDiceGame.java
+++ src/main/java/com/bettergames/BigDiceGame.java
@@ -16,25 +16,27 @@ public class BigDiceGame
                 return applesauce3(die1, die2, die3, die4, die5);
 
             case FiveOfAKind:
+                return applesauce4(die1, die2, die3, die4, die5);
+        }
+
+        return 0;
+    }
+
+    private static int applesauce4(int die1, int die2, int die3, int die4, int die5) {
+        int[] dice1 = {die1, die2, die3, die4, die5};
+        var counts2 = new int[6];
+
+        for (var die : dice1)
+        {
+            counts2[die - 1]++;
+        }
+
+        for (var i1 = 0; i1 != 6; i1++)
+        {
+            if (counts2[i1] == 5)
             {
-                int[] dice1 = {die1, die2, die3, die4, die5};
-                var counts2 = new int[6];
-                
-                for (var die : dice1)
-                {
-                    counts2[die - 1]++;
-                }
-
-                for (var i1 = 0; i1 != 6; i1++)
-                {
-                    if (counts2[i1] == 5)
-                    {
-                        int bigScore = 50;
-                        return bigScore;
-                    }
-                }
-
-                return 0;
+                int bigScore = 50;
+                return bigScore;
             }
         }
 
