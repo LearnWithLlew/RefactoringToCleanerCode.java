fe661d8c5ada4c3c957b0b53e454a3deae4158b7
Llewellyn Falco <llewellyn.falco@gmail.com>
2023-07-27 14:53:04 -0400

    . r move to fiveofakind score

--- src/main/java/com/bettergames/BigDiceGame.java
+++ src/main/java/com/bettergames/BigDiceGame.java
@@ -16,28 +16,7 @@ public class BigDiceGame
                 return FullHouseScore.applesauce3(die1, die2, die3, die4, die5);
 
             case FiveOfAKind:
-                return applesauce4(die1, die2, die3, die4, die5);
-        }
-
-        return 0;
-    }
-
-    private static int applesauce4(int die1, int die2, int die3, int die4, int die5) {
-        int[] dice1 = {die1, die2, die3, die4, die5};
-        var counts2 = new int[6];
-
-        for (var die : dice1)
-        {
-            counts2[die - 1]++;
-        }
-
-        for (var i1 = 0; i1 != 6; i1++)
-        {
-            if (counts2[i1] == 5)
-            {
-                int bigScore = 50;
-                return bigScore;
-            }
+                return FiveOfAKindScore.applesauce4(die1, die2, die3, die4, die5);
         }
 
         return 0;
---
+++ src/main/java/com/bettergames/FiveOfAKindScore.java
@@ -0,0 +1,21 @@
+package com.bettergames;
+
+public class FiveOfAKindScore {
+    static int applesauce4(int die1, int die2, int die3, int die4, int die5) {
+        int[] dice1 = {die1, die2, die3, die4, die5};
+        var counts2 = new int[6];
+
+        for (var die : dice1) {
+            counts2[die - 1]++;
+        }
+
+        for (var i1 = 0; i1 != 6; i1++) {
+            if (counts2[i1] == 5) {
+                int bigScore = 50;
+                return bigScore;
+            }
+        }
+
+        return 0;
+    }
+}
