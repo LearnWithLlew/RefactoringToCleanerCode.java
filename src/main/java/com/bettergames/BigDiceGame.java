bccbfbbb126b3a23d97f23b0308935cd2daa0b6d
Llewellyn Falco <llewellyn.falco@gmail.com>
2023-07-27 16:29:56 -0400

    ! r many-of-four

--- src/main/java/com/bettergames/BigDiceGame.java
+++ src/main/java/com/bettergames/BigDiceGame.java
@@ -5,16 +5,13 @@ public class BigDiceGame
     public static int calculateScore(ScoringType scoringType, int die1, int die2, int die3, int die4, int die5)
     {
         {
-            var scorer = new DiceScorer[]{new PairScore(), new TwoPairScore(), new FullHouseScore()};
+            var scorer = new DiceScorer[]{new PairScore(), new TwoPairScore(), new FullHouseScore(), new FiveOfAKindScore()};
             for (DiceScorer diceScorer : scorer) {
                 if (diceScorer.isScore(scoringType)) {
                     return diceScorer.calculateScore(die1, die2, die3, die4, die5);
                 }
 
             }
-        }
-        if (new FiveOfAKindScore().isScore(scoringType)) {
-            return new FiveOfAKindScore().calculateScore(die1, die2, die3, die4, die5);
         }
 
         return 0;
