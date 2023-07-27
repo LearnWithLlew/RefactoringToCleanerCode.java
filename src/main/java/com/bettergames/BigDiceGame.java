45f105286d813d4ec6bf4f95875c3022f60afb94
Llewellyn Falco <llewellyn.falco@gmail.com>
2023-07-27 16:26:44 -0400

    . r replace with var

--- src/main/java/com/bettergames/BigDiceGame.java
+++ src/main/java/com/bettergames/BigDiceGame.java
@@ -5,7 +5,7 @@ public class BigDiceGame
     public static int calculateScore(ScoringType scoringType, int die1, int die2, int die3, int die4, int die5)
     {
         {
-            PairScore scorer = new PairScore();
+            var scorer = new PairScore();
             if (scorer.isScore(scoringType)) {
                 return scorer.calculateScore(die1, die2, die3, die4, die5);
             }
