e309346cba92947521539f1aee92c87d06e64280
Llewellyn Falco <llewellyn.falco@gmail.com>
2023-07-27 14:56:08 -0400

    . r refactor the switch to include if statement

--- src/main/java/com/bettergames/BigDiceGame.java
+++ src/main/java/com/bettergames/BigDiceGame.java
@@ -4,19 +4,14 @@ public class BigDiceGame
 {
     public static int calculateScore(ScoringType scoringType, int die1, int die2, int die3, int die4, int die5)
     {
-        switch (scoringType)
-        {
-            case Pair:
-                return PairScore.applesauce(die1, die2, die3, die4, die5);
-
-            case TwoPair:
-                return TwoPairScore.applesauce2(die1, die2, die3, die4, die5);
-
-            case FullHouse:
-                return FullHouseScore.applesauce3(die1, die2, die3, die4, die5);
-
-            case FiveOfAKind:
-                return FiveOfAKindScore.applesauce4(die1, die2, die3, die4, die5);
+        if (scoringType == ScoringType.Pair) {
+            return PairScore.applesauce(die1, die2, die3, die4, die5);
+        } else if (scoringType == ScoringType.TwoPair) {
+            return TwoPairScore.applesauce2(die1, die2, die3, die4, die5);
+        } else if (scoringType == ScoringType.FullHouse) {
+            return FullHouseScore.applesauce3(die1, die2, die3, die4, die5);
+        } else if (scoringType == ScoringType.FiveOfAKind) {
+            return FiveOfAKindScore.applesauce4(die1, die2, die3, die4, die5);
         }
 
         return 0;
