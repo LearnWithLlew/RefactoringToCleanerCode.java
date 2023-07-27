20ba38af2608d143e79e1d0fdfdd14170a6a5977
Llewellyn Falco <llewellyn.falco@gmail.com>
2023-07-27 14:45:41 -0400

    . r moe to pair score

--- src/main/java/com/bettergames/BigDiceGame.java
+++ src/main/java/com/bettergames/BigDiceGame.java
@@ -7,7 +7,7 @@ public class BigDiceGame
         switch (scoringType)
         {
             case Pair:
-                return applesauce(die1, die2, die3, die4, die5);
+                return PairScore.applesauce(die1, die2, die3, die4, die5);
 
             case TwoPair:
                 return applesauce2(die1, die2, die3, die4, die5);
@@ -110,23 +110,4 @@ public class BigDiceGame
         return 0;
     }
 
-    private static int applesauce(int die1, int die2, int die3, int die4, int die5) {
-        final int value = 2;
-        int[] tallies1;
-        tallies1 = new int[6];
-        tallies1[die1 - 1]++;
-        tallies1[die2 - 1]++;
-        tallies1[die3 - 1]++;
-        tallies1[die4 - 1]++;
-        tallies1[die5 - 1]++;
-        for (var i = 6 - 1; i >= 0; i--)
-        {
-            if (tallies1[i] >= value)
-            {
-                return (i + 1) * value;
-            }
-        }
-
-        return 0;
-    }
 }
---
+++ src/main/java/com/bettergames/PairScore.java
@@ -0,0 +1,21 @@
+package com.bettergames;
+
+public class PairScore {
+    static int applesauce(int die1, int die2, int die3, int die4, int die5) {
+        final int value = 2;
+        int[] tallies1;
+        tallies1 = new int[6];
+        tallies1[die1 - 1]++;
+        tallies1[die2 - 1]++;
+        tallies1[die3 - 1]++;
+        tallies1[die4 - 1]++;
+        tallies1[die5 - 1]++;
+        for (var i = 6 - 1; i >= 0; i--) {
+            if (tallies1[i] >= value) {
+                return (i + 1) * value;
+            }
+        }
+
+        return 0;
+    }
+}
