84ff8294efdf6f265215e8760dcb3b62c28c7f84
Llewellyn Falco <llewellyn.falco@gmail.com>
2023-07-27 16:29:11 -0400

    ! r many-of-two

--- src/main/java/com/bettergames/BigDiceGame.java
+++ src/main/java/com/bettergames/BigDiceGame.java
@@ -5,16 +5,13 @@ public class BigDiceGame
     public static int calculateScore(ScoringType scoringType, int die1, int die2, int die3, int die4, int die5)
     {
         {
-            var scorer = new DiceScorer[]{new PairScore()};
+            var scorer = new DiceScorer[]{new PairScore(), new TwoPairScore()};
             for (DiceScorer diceScorer : scorer) {
                 if (diceScorer.isScore(scoringType)) {
                     return diceScorer.calculateScore(die1, die2, die3, die4, die5);
                 }
 
             }
-        }
-        if (new TwoPairScore().isScore(scoringType)) {
-            return new TwoPairScore().calculateScore(die1, die2, die3, die4, die5);
         }
         if (new FullHouseScore().isScore(scoringType)) {
             return new FullHouseScore().calculateScore(die1, die2, die3, die4, die5);
